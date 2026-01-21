b6728e6b9c1bedba6e65a1d0a39986b65434f756
kiranraoboinapally <kiranraoboinapally@gmail.com>
2026-01-21 19:02:04 +0530

    removed all backend2 folder of springboot related

--- backend2/src/main/java/com/expense/tracker/repository/ExpenseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.expense.tracker.repository;
-
-import com.expense.tracker.model.Expense;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface ExpenseRepository extends JpaRepository<Expense, Long> {
-    List<Expense> findByUserId(Long userId);
-}
